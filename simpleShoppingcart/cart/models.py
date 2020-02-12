d8191bb113404450a16fb0dc1a477de8d58df6d1
MeshackKipkorir <meshackkipkorir105@gmail.com>
2020-02-12 00:45:52 -0500

    modelled addToCart functionality

--- simpleShoppingcart/cart/models.py
+++ simpleShoppingcart/cart/models.py
@@ -32,3 +32,27 @@ def update_profile_signal(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
     instance.profile.save()
+
+class OrderItem(models.Model):
+    product = models.OneToOneField(AddProductModel,on_delete=models.SET_NULL,null=True)
+    is_ordered = models.BooleanField(default=False)
+    date_added = models.DateTimeField(auto_now=True)
+    date_ordered = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.product.name
+
+class Order(models.Model):
+    ref_code = models.CharField(max_length=15)    
+    owner = models.ForeignKey(Profile,on_delete=models.SET_NULL,null=True)
+    is_ordered = models.BooleanField(default=False)
+    items = models.ManyToManyField(OrderItem)
+    date_ordered = models.DateTimeField(auto_now=True)
+
+    def get_cart_items(self):
+        return self.items.all()
+    def get_cart_total(self):
+        return sum([item.product.product_price for item in self.items.all()])
+
+        def __str__(self):
+            return '{0} - {1}'.format(self.owner,self.ref_code)
